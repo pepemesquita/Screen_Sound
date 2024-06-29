03fa6a7b41fd3fdc8a6a043e49c56b3be542753c
Pedro <phlmesquita@inf.ufpel.edu.br>
2024-06-29 15:40:46 -0300

    Se aventurando no C#

---
+++ Adivinhacao.cs
@@ -0,0 +1,26 @@
+
+// Random rnd = new Random();
+// int sorteado = rnd.Next(1, 100);
+// int contador = 1;
+// int numeroChutado;
+
+// Console.WriteLine("\nBem vindo ao jogo de adivinhação! Tente adivinhar um número de 1 até 100!");
+
+
+// do{
+
+//     Console.Write($"\nTentativa {contador}, qual seu chute? ");
+//     numeroChutado = int.Parse(Console.ReadLine()!); 
+    
+
+//     if(numeroChutado < sorteado){
+//          Console.WriteLine("\nO número sorteado é maior!");
+//     }else{
+//         Console.WriteLine("\nO número sorteado é menor!");
+//     }
+
+//     contador++;
+
+// } while (numeroChutado != sorteado);
+
+// Console.WriteLine($"\nParabéns, você ganhou em {contador} tentativas, o número era {sorteado}.");
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿
+void ExibirLogo(){
+    Console.WriteLine(@"
+    
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+
+}
+
+void ExibirMensagemDeBoasVindas(){
+    string mensagemBoasVindas = ("Boas Vindas ao Screen Sound!\n");    
+    Console.WriteLine(mensagemBoasVindas);
+   
+}
+
+void ExibirOpcoesDoMenu(){
+    Console.WriteLine("Digite 1 para registrar uma banda.");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas.");
+    Console.WriteLine("Digite 3 para avaliar uma banda.");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda.");
+    Console.WriteLine("Digite -1 para sair.");
+
+    Console.Write("\nDigite sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica){
+        case 1:
+            RegistrarBanda();
+            break;
+        
+        case 2:
+            Console.WriteLine($"\nA opção escolhida foi {opcaoEscolhidaNumerica}");
+            break;
+        
+        case 3:
+            Console.WriteLine($"\nA opção escolhida foi {opcaoEscolhidaNumerica}");
+            break;
+        
+        case 4:
+            Console.WriteLine($"\nA opção escolhida foi {opcaoEscolhidaNumerica}");
+            break;
+        
+        case -1:
+            Console.WriteLine($"\nA opção escolhida foi {opcaoEscolhidaNumerica}");
+            break;
+
+        default:
+            Console.WriteLine("\nOpção inválida!");
+            break;
+    }
+}
+
+
+void RegistrarBanda(){
+
+}
+
+
+ExibirLogo();
+ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
